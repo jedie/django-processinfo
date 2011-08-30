30eef59a58bfb60f5746e5e7c4b507a0f306554a
Jens Diemer <github.com@jensdiemer.de>
2011-08-30 16:26:47 +0300

    add info

--- setup.py
+++ setup.py
@@ -19,6 +19,8 @@ from django_processinfo import VERSION_STRING
 PACKAGE_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
+# convert creole to ReSt on-the-fly, see also:
+# https://code.google.com/p/python-creole/wiki/UseInSetup
 try:
     from creole.setup_utils import GetLongDescription
 except ImportError:
